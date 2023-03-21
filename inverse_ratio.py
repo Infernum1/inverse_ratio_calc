c660cd09988f167e6f8f4787db061c97021c2873
oBreez <81740112+oBreez@users.noreply.github.com>
2023-03-21 21:07:55 +0530

    some improvements to make it easier to use

--- inverse_ratio.py
+++ inverse_ratio.py
@@ -1,6 +1,6 @@
 import math
 from triangle_drawer import Triangle
-
+from fuzzywuzzy import process
 func = input(("Enter the trigonometric ratio (sin/sine, cos/cosine or tan/tangent): "))
 
 sides = {}
@@ -8,15 +8,24 @@ sides.update(dict.fromkeys(["sin", "sine"], ["perpendicular", "hypotenuse"]))
 sides.update(dict.fromkeys(["cos", "cosine"], ["base", "hypotenuse"]))
 sides.update(dict.fromkeys(["tan", "tangent"], ["perpendicular", "base"])) #could probably improve this
 
-if func not in sides:
+list_of_ratios = list(sides)
+searched_ratio = process.extract(func, list_of_ratios)
+
+if searched_ratio[0][1] >= 60:
+    print(f"Not a valid ratio, perhaps you made a typo but meant '{searched_ratio[0][0]}'")
+    exit()
+
+elif searched_ratio[0][1] < 60:
     print("Please ONLY type the ratio as hinted in the prompt")
     exit()
 
 chosen_sides_by_ratio = sides[func] 
-
-side1 = eval((input(f"Input {chosen_sides_by_ratio[0]} measurement: ")))
-side2 = eval((input(f"Input {chosen_sides_by_ratio[1]} measurement: ")))
-
+try:
+    side1 = eval((input(f"Input {chosen_sides_by_ratio[0]} measurement: ")))
+    side2 = eval((input(f"Input {chosen_sides_by_ratio[1]} measurement: ")))
+except NameError:
+    print("Make sure you ONLY input the integer/decimal value of the measure of the side.")
+    exit()
 decimal_value = side1/side2
 
 ratios = {}
